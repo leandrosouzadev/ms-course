eafd737b047764dd14db8432927589d579e394b4
Leandro Souza <53835002+leandrosouzadev@users.noreply.github.com>
2023-05-15 17:42:54 -0300

    Ribbon load balancing

--- hr-worker/src/main/java/br/com/elegacy/hrworker/entities/Worker.java
+++ hr-worker/src/main/java/br/com/elegacy/hrworker/entities/Worker.java
@@ -2,11 +2,12 @@ package br.com.elegacy.hrworker.entities;
 
 import java.io.Serializable;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Data;
 
 @Data
