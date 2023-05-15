a2ad6695d51bd4ed4a155d72146365c9b783d2cf
Leandro Souza <53835002+leandrosouzadev@users.noreply.github.com>
2023-05-15 12:07:57 -0300

    Feign Client

--- hr-payroll/src/main/java/br/com/elegacy/payroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/elegacy/payroll/services/PaymentService.java
@@ -1,33 +1,20 @@
 package br.com.elegacy.payroll.services;
 
-import java.net.URI;
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import br.com.elegacy.payroll.entities.Payment;
 import br.com.elegacy.payroll.entities.Worker;
+import br.com.elegacy.payroll.feignclients.WorkerFeignClient;
 
 @Service
-public class PaymentService {
-	
-	@Value("${hr-worker.host}")
-	private String workerHost;
+public class PaymentService {	
 	
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 
-	public Payment getPayment(Long workerId, int days) {
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", String.valueOf(workerId));
-		
-		String url = String.format("%s/workers/{id}", workerHost);		
-		Worker worker = restTemplate.getForObject(url, Worker.class, uriVariables);
-		
+	public Payment getPayment(Long workerId, int days) {					
+		Worker worker = workerFeignClient.findById(workerId).getBody();		
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 
